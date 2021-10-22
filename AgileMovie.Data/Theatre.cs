a07733ba553c3e49ee11b58051697a03f18cc2d5
Taylor <taylor.randol@gmail.com>
2021-10-22 11:56:43 -0400

    theatre update

--- AgileMovie.Data/Theatre.cs
+++ AgileMovie.Data/Theatre.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,14 @@ namespace AgileMovie.Data
 {
     public class Theatre
     {
+        [Key]
+        public int TheatreId { get; set; }
 
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+
+        public decimal TicketPrice { get; set; }
+
+        public List<Movie> SelectMovie { get; set; }
     }
 }
