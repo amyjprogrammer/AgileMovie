2f77446aaae68b7b3168d017b1ebf9443b58f831
Emily Kampf <emilykampf@gmail.com>
2021-10-22 22:51:54 -0500

    Created Controller&Service

---
+++ AgileMovie.Services/MovieService.cs
@@ -0,0 +1,107 @@
+﻿using AgileMovie.Data;
+using AgileMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgileMovie.Services
+{
+    public class MovieService
+    {
+        private readonly int _userId;
+
+        public MovieService(int userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateMovie(MovieCreate model)
+        {
+            var entity = new Movie()
+            {
+                MovieId = _userId,
+                MovieName = model.MovieName,
+                MovieDescription = model.MovieDescription,
+                Genre = model.Genre
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Movies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<MovieListItem> GetMovie()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Movies
+                        .Where(e => e.MovieId == _userId)
+                        .Select(
+                             e =>
+                              new MovieListItem
+                              {
+                                  MovieId = e.MovieId,
+                                  MovieName = e.MovieName,
+                                  MovieDescription = e.MovieDescription,
+                                  Genre = e.Genre
+                              });
+                              
+                return query.ToArray();
+            }
+        }
+
+        public MovieDetail GetMovieById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Movies
+                    .Single(e => e.MovieId == id && e.MovieId == _userId);
+                return
+                    new MovieDetail
+                    {
+                        MovieId = entity.MovieId,
+                        MovieName = entity.MovieName,
+                        MovieDescription = entity.MovieDescription,
+                        Genre = entity.Genre
+                    };
+            }
+        }
+        public bool UpdateMovie(MovieEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Movies
+                        .Single(e => e.MovieId == model.MovieId);
+
+                entity.MovieName = model.MovieName;
+                entity.MovieDescription = model.MovieDescription;
+                entity.Genre = model.Genre;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteMovie(int movieId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Movies
+                    .Single(e => e.MovieId == movieId);
+
+                ctx.Movies.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+    }
+}
---
+++ AgileMovie.WebAPI/Controllers/MovieController.cs
@@ -0,0 +1,73 @@
+﻿using AgileMovie.Models;
+using AgileMovie.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AgileMovie.WebAPI.Controllers
+{
+    [Authorize]
+    public class MovieController : ApiController
+    {
+        private MovieService CreateMovieService()
+        {
+            var userId = int.Parse(User.Identity.GetUserId());
+            var movieService = new MovieService(userId);
+            return movieService;
+        }
+        public IHttpActionResult Get()
+        {
+            MovieService movieService = CreateMovieService();
+            var movies = movieService.GetMovie();
+            return Ok(movies);
+        }
+
+        public IHttpActionResult Post(MovieCreate movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateMovieService();
+
+            if (!service.CreateMovie(movie))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //Get Method by Id
+        public IHttpActionResult Get(int id)
+        {
+            MovieService movieService = CreateMovieService();
+            var movie = movieService.GetMovieById(id);
+            return Ok(movie);
+        }
+
+        public IHttpActionResult Put(MovieEdit movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateMovieService();
+
+            if (!service.UpdateMovie(movie))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateMovieService();
+
+            if (!service.DeleteMovie(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
